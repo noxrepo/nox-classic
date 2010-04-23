26d3f71c055fd6a8f68a38837b73c7065864d702
zarifis <kyr.zarifis@gmail.com>
2010-04-23 05:06:55 -0700

    json utility functions

---
+++ src/include/json-util.hh
@@ -0,0 +1,42 @@
+/* Copyright 2008 (C) Nicira, Inc.
+ *
+ * This file is part of NOX.
+ *
+ * NOX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NOX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NOX.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef JSON_HH
+#define JSON_HH 1
+
+#include <list>
+#include <string>
+
+#include <boost/shared_array.hpp>
+
+#include "json_object.hh"
+
+namespace vigil {
+namespace json {
+
+/*
+ * JSON convenience functions component developers may find useful
+ * while accessing a JSON object tree passed for them.
+ */
+ 
+/* Load a JSON file. */
+json_object* load_document(const std::string& file);
+
+}
+}
+
+#endif
---
+++ src/lib/json-util.cc
@@ -0,0 +1,52 @@
+/* Copyright 2008 (C) Nicira, Inc.
+ *
+ * This file is part of NOX.
+ *
+ * NOX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NOX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NOX.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "json-util.hh"
+#include <fstream>
+
+using namespace std;
+
+namespace vigil {
+namespace json {
+
+json_object* load_document(const string& file) {
+    
+    // TODO: Add checks, return error etc
+    int len;
+    char* jsonstr;
+    ifstream in;
+    json_object* jo;
+    
+    in.open(file.c_str());
+    // get conf file len
+    in.seekg(0, ios::end);
+    len = in.tellg();
+    in.seekg(0, ios::beg);
+    // alloc
+    jsonstr = new char [len];
+    in.read(jsonstr,len);
+    in.close();
+    boost::shared_array<uint8_t> str;
+    str.reset(new uint8_t[len+1]);
+    strcpy((char *) str.get(),jsonstr);
+    jo = new json_object(str.get(), len);
+    
+    return jo;
+}
+
+}
+}
